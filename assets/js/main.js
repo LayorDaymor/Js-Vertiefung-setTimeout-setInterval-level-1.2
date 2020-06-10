935dd834ee600fa2ea410db795e1ed43589d9c8e
LayorDaymor <ahmad.karbouj98@gmail.com>
2020-06-10 22:31:38 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+const body=document.body;
+const startBtn=document.getElementById("btn");
+const zeit=document.querySelector(".zeit")
+
+let countDown=100
+startBtn.addEventListener("click",e=>{
+  var x = setInterval(() => {
+        countDown--
+        zeit.textContent=`${countDown}%`
+        if (countDown<=0) {
+        clearInterval(x)
+        zeit.textContent+=` your Downloud ist Ready`
+        startBtn.textContent=":)"
+        body.style.background="green"
+        body.style.color="black"
+        zeit.style.background="black"
+        startBtn.style.background="black"
+        }
+        
+    }, 30);
+})
